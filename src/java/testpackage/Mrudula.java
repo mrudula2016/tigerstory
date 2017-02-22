845ee99e9362d358ea32818ac1c2254e15a7304c
mrudula <mrudula@pinkhome.Home>
2017-02-22 23:18:03 +0000

    made some changes

---
+++ src/java/testpackage/Mrudula.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testpackage;
+
+/**
+ *
+ * @author mrudula
+ */
+public class Mrudula {
+    static String name="mrudula";
+    public static void main(String[] args) {
+        System.out.println(name);
+    }
+    
+}
